e420719baa1c7a3cef299afb0cf4d20aa3cbd2f8
Kevalkumar <kgi@mm-software.com>
2026-01-15 12:43:08 +0530

    remove authorization

--- source/Aas.TwinEngine.Plugin.RelationalDatabase/Program.cs
+++ source/Aas.TwinEngine.Plugin.RelationalDatabase/Program.cs
@@ -54,7 +54,6 @@ public static class Program
 
         _ = app.UseExceptionHandler();
         _ = app.UseHttpsRedirection();
-        _ = app.UseAuthorization();
 
         app.UseCorsServices();
         _ = app.UseOpenApi(c => c.PostProcess = (d, _) => d.Servers.Clear());
